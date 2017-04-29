1c8214c92419be0ad806d6d2c63fff3c1efe2eb2
SEEKASIA\andrewkhaw <andrewkhaw@seekasia.com>
2017-04-29 15:50:58 +0800

    add screamFactor

--- client/cheratingBeachViewPage/cheratingBeachViewPage.js
+++ client/cheratingBeachViewPage/cheratingBeachViewPage.js
@@ -8,79 +8,106 @@ import {
   CATEGORY_LIST
 } from '../components/constants/FormData';
 
-const dataSet ={
-  '6':{
-    'isRain':false,
-    'isSun':true
+const dataSet = {
+  '6': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '7':{
-    'isRain':false,
-    'isSun':true
+  '7': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '8':{
-    'isRain':false,
-    'isSun':true
+  '8': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '9':{
-    'isRain':true,
-    'isSun':false
+  '9': {
+    'uvLevel': 2,
+    'isRain': true,
+    'isSun': false
   },
-  '10':{
-    'isRain':false,
-    'isSun':true
+  '10': {
+    'uvLevel': 5,
+    'isRain': false,
+    'isSun': true
   },
-  '11':{
-    'isRain':false,
-    'isSun':true
+  '11': {
+    'uvLevel': 8,
+    'isRain': false,
+    'isSun': true
   },
-  '12':{
-    'isRain':false,
-    'isSun':true
+  '12': {
+    'uvLevel': 11,
+    'isRain': false,
+    'isSun': true
   },
-  '13':{
-    'isRain':true,
-    'isSun':false
+  '13': {
+    'uvLevel': 11,
+    'isRain': true,
+    'isSun': false
   },
-  '14':{
-    'isRain':true,
-    'isSun':false
+  '14': {
+    'uvLevel': 11,
+    'isRain': true,
+    'isSun': false
   },
-  '15':{
-    'isRain':false,
-    'isSun':true
+  '15': {
+    'uvLevel': 9,
+    'isRain': false,
+    'isSun': true
   },
-  '16':{
-    'isRain':true,
-    'isSun':false
+  '16': {
+    'uvLevel': 6,
+    'isRain': true,
+    'isSun': false
   },
-  '17':{
-    'isRain':true,
-    'isSun':false
+  '17': {
+    'uvLevel': 3,
+    'isRain': true,
+    'isSun': false
   },
-  '18':{
-    'isRain':false,
-    'isSun':true
+  '18': {
+    'uvLevel': 1,
+    'isRain': false,
+    'isSun': true
   },
-  '19':{
-    'isRain':false,
-    'isSun':true
+  '19': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '20':{
-    'isRain':false,
-    'isSun':true
+  '20': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '21':{
-    'isRain':false,
-    'isSun':true
+  '21': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   },
-  '22':{
-    'isRain':false,
-    'isSun':true
+  '22': {
+    'uvLevel': 0,
+    'isRain': false,
+    'isSun': true
   }
 }
 
 const DEFAULTMINMONTHS = 6;
 const DEFAULTMAXMONTHS = 22;
+
+function getScreamFactor() {
+  if (dataSet[Session.get("slider")].uvLevel >= 7) {
+    return "30";
+  } else if (dataSet[Session.get("slider")].uvLevel >= 4) {
+    return "20";
+  } else if (dataSet[Session.get("slider")].uvLevel >= 3) {
+    return "10";
+  } else { return "0"; }
+}
 
 if (Meteor.isClient) {
   Session.setDefault("slider", DEFAULTMINMONTHS);
@@ -91,6 +118,23 @@ if (Meteor.isClient) {
     },
     'isSun': function () {
       return dataSet[Session.get("slider")].isSun;
+    },
+    'suncreamFactor': function () {
+      // if (dataSet[Session.get("slider")].uvLevel >= 11) {
+      //   return "extreme";
+      // } else if (dataSet[Session.get("slider")].uvLevel >= 8) {
+      //   return "very high";
+      // } else if (dataSet[Session.get("slider")].uvLevel >= 6) {
+      //   return "high";
+      // } else if (dataSet[Session.get("slider")].uvLevel >= 3) {
+      //   return "moderate";
+      // } else if (dataSet[Session.get("slider")].uvLevel >= 0) {
+      //   return "low";
+      // }
+      return getScreamFactor();
+    },
+    'suncreamFactorIs': function (factor) {
+      return getScreamFactor() === factor;
     }
   });
 
